f652232891280784fd7304a0c1e8f4d6f7ac98cf
Insight <whdtune@gmail.com>
2017-04-02 16:06:56 +0800

    direction description method updated

---
+++ src/TankGame_v4/Members.java
@@ -0,0 +1,272 @@
+package TankGame_v4;
+
+import java.awt.*;
+import java.util.Vector;
+
+/**
+ * Created by Insight on 2017-04-02.
+ */
+class Tank {
+    public static final int DIR_POSITIVE = 1;   // right
+    public static final int DIR_NEGATIVE = -1;  // left
+    public static final int DIR_V = 2;          // vertical
+    public static final int DIR_H = -2;         // horizontal
+
+    public static final int MAX_BOMB_NUM = 5;
+    public static final int WIDTH = 24;
+    public static final int HEIGHT = 36;
+
+    int x = 0;      // x of center
+    int y = 0;      // y of center
+    int speed = 2;
+    int dirVH;
+    int dirPosNeg;
+    Color color;
+
+    public boolean isAlive = true;
+    public Vector<Bomb> bombs = new Vector<>();
+
+    public Tank(int x, int y) {
+        this.x = x;
+        this.y = y;
+        dirVH = DIR_V;
+        dirPosNeg = DIR_NEGATIVE;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getDirVH() {
+        return dirVH;
+    }
+
+    public int getDirPosNeg() {
+        return dirPosNeg;
+    }
+
+    public void setDirVH(int dirVH) {
+        this.dirVH = dirVH;
+    }
+
+    public void setDirPosNeg(int dirPosNeg) {
+        this.dirPosNeg = dirPosNeg;
+    }
+
+    // move one step
+    public void step() {
+        if (dirVH == DIR_V) {
+            y += speed * dirPosNeg;
+        } else if (dirVH == DIR_H) {
+            x += speed * dirPosNeg;
+        }
+    }
+
+    public Bomb shoot() {
+        // get bomb initial position
+        int bombX = 0;
+        int bombY = 0;
+        switch (dirVH) {
+            case DIR_V:
+                bombX = x;
+                bombY = y + dirPosNeg*HEIGHT/2;
+                break;
+            case DIR_H:
+                bombX = x + dirPosNeg*HEIGHT/2;
+                bombY = y;
+                break;
+        }
+
+        // add bomb instance
+        Bomb newBomb = new Bomb(bombX, bombY, this.dirVH, this.dirPosNeg, this.color);
+        newBomb.isAlive = true;
+        bombs.add(newBomb);
+        return newBomb;
+    }
+}
+
+class HeroTank extends Tank {
+    public HeroTank(int x, int y) {
+        super(x, y);
+
+        setColor(Color.CYAN);
+    }
+}
+
+class EnemyTank extends Tank implements Runnable {
+    private int CONTINUOUS_STEP_NUM = 5;    // number of continuous march sequence
+    private int STEP_INTERVAL = 150;        // interval of consequent marches
+    private Vector<EnemyTank> panelEnemyTanks;
+
+    public EnemyTank(int x, int y) {
+        super(x, y);
+
+        setColor(Color.YELLOW);
+        // initial direction -- down
+        setDirVH(DIR_V);
+        setDirPosNeg(DIR_POSITIVE);
+    }
+
+    public void passEnemyTanks(Vector<EnemyTank> ets) {
+        panelEnemyTanks = ets;
+    }
+
+//    // check collision with other enemyTanks
+//    public boolean detectCollision() {
+//        if (panelEnemyTanks != null) {
+//            for (EnemyTank et : panelEnemyTanks) {
+//                if (et != this) {
+//                    if (et.dirVH == DIR_V && this.dirVH == DIR_V) {
+//                        if ((et.x-this.x < Tank.WIDTH) && (et.y-this.y < Tank.HEIGHT)) {
+//                            return true;
+//                        }
+//                    } else if (et.dirVH == DIR_H && this.dirVH == DIR_H) {
+//                        if ((et.x-this.x < Tank.HEIGHT) && (et.y-this.y < Tank.WIDTH)) {
+//                            return true;
+//                        }
+//                    } else if (et.dirVH * this.dirVH < 0) {     // if their directions are orthogonal
+//                        if ((et.x-this.x < Tank.WIDTH/2+Tank.HEIGHT/2)
+//                                && (et.y-this.y < Tank.WIDTH/2+Tank.HEIGHT/2)) {
+//                            return true;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        return false;
+//    }
+
+    @Override
+    public void run() {
+        while (isAlive) {
+            switch (dirVH) {
+                case DIR_V:
+                    for (int i = 0; i < CONTINUOUS_STEP_NUM; i++) {
+                        // up or down
+                        if ((dirPosNeg==DIR_NEGATIVE && y>=HEIGHT/2+speed)
+                                || (dirPosNeg==DIR_POSITIVE && y+HEIGHT/2+speed<=MainFrame.W_HEIGHT)) {
+                            y += speed * dirPosNeg;
+                        }
+                        try {
+                            Thread.sleep(STEP_INTERVAL);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                case DIR_H:
+                    for (int i = 0; i < CONTINUOUS_STEP_NUM; i++) {
+                        // left or right
+                        if ((dirPosNeg==DIR_NEGATIVE && x>=WIDTH/2+speed)    // up
+                                || (dirPosNeg==DIR_POSITIVE && x+WIDTH/2+speed<=MainFrame.W_WIDTH)) {
+                            x += speed * dirPosNeg;
+                        }
+                        try {
+                            Thread.sleep(STEP_INTERVAL);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+            }
+
+            // next direction
+            if (Math.random() >= 0.5) {
+                dirPosNeg = DIR_POSITIVE;
+            } else {
+                dirPosNeg = DIR_NEGATIVE;
+            }
+            if (Math.random() >= 0.5) {
+                dirVH = DIR_V;
+            } else {
+                dirVH = DIR_H;
+            }
+
+            // shoot bomb
+            if (bombs.size() < MAX_BOMB_NUM) {
+                Bomb bomb = shoot();
+                Thread bombThread = new Thread(bomb);
+                bombThread.start();
+            }
+        }
+    }
+}
+
+class Bomb implements Runnable {
+    public static final int DIR_POSITIVE = 1;   // right
+    public static final int DIR_NEGATIVE = -1;  // left
+    public static final int DIR_V = 2;          // vertical
+    public static final int DIR_H = -2;         // horizontal
+
+    private int x;
+    private int y;
+
+    private int speed = 5;
+    private Color color;
+    private int dirVH;
+    private int dirPosNeg;
+
+    public boolean isAlive = false;
+
+    public Bomb(int x, int y, int dirVH, int dirPosNeg, Color color) {
+        this.x = x;
+        this.y = y;
+        this.dirVH = dirVH;
+        this.dirPosNeg = dirPosNeg;
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getDirVH() {
+        return dirVH;
+    }
+
+    public int getDirPosNeg() {
+        return dirPosNeg;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // judge whether alive
+            if (x < 0 | x > MainFrame.W_WIDTH | y < 0 | y > MainFrame.W_HEIGHT) {
+                isAlive = false;
+                break;
+            } else {
+                switch (dirVH) {
+                    case DIR_V:
+                        y += speed * dirPosNeg;
+                        break;
+                    case DIR_H:
+                        x += speed * dirPosNeg;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ src/TankGame_v4/TankGame.java
@@ -0,0 +1,277 @@
+package TankGame_v4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Vector;
+
+/**
+ * Created by Insight on 2017-04-02.
+ * function:
+ * 1. draw hero tank and enemy tanks
+ * 2. hero tank can shoot multiple bombs
+ * 3. when bombs hit enemy tanks, they disappear together
+ * 4. enemy tanks can move randomly within the boundary
+ * 5. enemy tanks can shoot bombs, heroTank hit by enemyBomb will die
+ * 6. enemy tanks can avoid collision with each other
+ */
+public class TankGame {
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.showUp();
+    }
+}
+
+class MainFrame extends JFrame {
+    public static final int W_WIDTH = 800;
+    public static final int W_HEIGHT = 600;
+
+    private MainPanel mainPanel;
+    private Thread panelThread;
+
+    public MainFrame() {
+        mainPanel = new MainPanel();
+        this.add(mainPanel);
+        this.addKeyListener(mainPanel);
+
+        panelThread = new Thread(mainPanel);
+        panelThread.start();
+    }
+
+    public void showUp() {
+        this.setSize(W_WIDTH, W_HEIGHT);
+        this.setResizable(false);
+        this.setLocationByPlatform(true);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
+
+class MainPanel extends JPanel implements KeyListener, Runnable {
+    private HeroTank heroTank;
+    private Vector<EnemyTank> enemyTanks;
+    private int enemyNum = 10;
+
+    public MainPanel() {
+        // initialize hero tank
+        heroTank = new HeroTank(MainFrame.W_WIDTH /2, MainFrame.W_HEIGHT - 2 * Tank.WIDTH);
+
+        // initialize enemy tanks
+        enemyTanks = new Vector<>();
+        for (int i = 0; i < enemyNum; i++) {
+            int et_x = MainFrame.W_WIDTH/(2*enemyNum) + i*MainFrame.W_WIDTH/enemyNum;
+            int et_y = Tank.HEIGHT / 2;
+            EnemyTank et = new EnemyTank(et_x, et_y);
+            enemyTanks.add(et);
+        }
+
+        for (EnemyTank et : enemyTanks) {
+            et.passEnemyTanks(enemyTanks);
+
+            Thread enemyThread = new Thread(et);
+            enemyThread.start();
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        // background
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, MainFrame.W_WIDTH, MainFrame.W_HEIGHT);
+
+        // draw living tanks
+        if (heroTank.isAlive) {
+            drawTank(heroTank, g);
+        }
+        for (int i = 0; i < enemyTanks.size(); i++) {
+            EnemyTank enemyTank = enemyTanks.get(i);
+            if (enemyTank.isAlive) {
+                drawTank(enemyTank, g);
+            }
+        }
+
+        // draw living bombs of heroTank
+        g.setColor(heroTank.getColor());
+        for (int i = 0; i < heroTank.bombs.size(); i++) {
+            Bomb thisBomb = heroTank.bombs.get(i);
+            if (thisBomb.isAlive) {
+                g.drawRect(thisBomb.getX(), thisBomb.getY(), 1, 1);
+            }
+        }
+
+        // draw living bombs of enemy tank
+        for (int i = 0; i < enemyTanks.size(); i++) {
+            EnemyTank enemyTank = enemyTanks.get(i);
+            g.setColor(enemyTank.getColor());
+            for (int j = 0; j < enemyTank.bombs.size(); j++) {
+                Bomb thisBomb = enemyTank.bombs.get(j);
+                if (thisBomb.isAlive) {
+                    g.drawRect(thisBomb.getX(), thisBomb.getY(), 1, 1);
+                }
+            }
+        }
+    }
+
+    /* draw a tank at specific position */
+    private void drawTank(Tank tank, Graphics g) {
+        int x = tank.getX();
+        int y = tank.getY();
+        g.setColor(tank.getColor());
+
+        int dirVH = tank.getDirVH();
+        int dirPosNeg = tank.getDirPosNeg();
+        switch (dirVH) {
+            case Tank.DIR_V:
+                g.fill3DRect(x - Tank.WIDTH/4, y - Tank.WIDTH/2, Tank.WIDTH/2, Tank.WIDTH, false);      // center rect
+                g.fillOval(x - Tank.WIDTH/4, y - Tank.WIDTH/4, Tank.WIDTH/2, Tank.WIDTH/2);
+                g.fill3DRect(x - Tank.WIDTH/2, y - Tank.HEIGHT/2, Tank.WIDTH/4, Tank.HEIGHT, false);    // left rect
+                g.fill3DRect(x + Tank.WIDTH/4, y - Tank.HEIGHT/2, Tank.WIDTH/4, Tank.HEIGHT, false);    // right rect
+                g.drawLine(x, y, x, y +  dirPosNeg*Tank.HEIGHT/2);
+                break;
+            case Tank.DIR_H:
+                g.fill3DRect(x - Tank.WIDTH/2, y - Tank.WIDTH/4, Tank.WIDTH, Tank.WIDTH/2, false);      // center rect
+                g.fillOval(x - Tank.WIDTH/4, y - Tank.WIDTH/4, Tank.WIDTH/2, Tank.WIDTH/2);             // oval
+                g.fill3DRect(x - Tank.HEIGHT/2, y - Tank.WIDTH/2, Tank.HEIGHT, Tank.WIDTH/4, false);    // upper rect
+                g.fill3DRect(x - Tank.HEIGHT/2, y + Tank.WIDTH/4, Tank.HEIGHT, Tank.WIDTH/4, false);    // lower rect
+                g.drawLine(x, y, x + dirPosNeg*Tank.HEIGHT/2, y);
+                break;
+        }
+    }
+
+    // check if a tank is hit by a bomb of enemy
+    private void updateHit(Tank tank, Bomb bomb) {
+        if (tank.isAlive && bomb.isAlive) {
+            // check if heroTank's bomb hit enemy tanks
+            switch (tank.getDirVH()) {
+                case Tank.DIR_V:
+                    if (bomb.getX()-tank.getX() < Tank.WIDTH/2
+                            && bomb.getX()-tank.getX() > -Tank.WIDTH/2
+                            && bomb.getY()-tank.getY() < Tank.HEIGHT/2
+                            && bomb.getY()-tank.getY() > -Tank.HEIGHT/2) {
+                        bomb.isAlive = false;
+                        tank.isAlive = false;
+                    }
+                    break;
+                case Tank.DIR_H:
+                    if (bomb.getX()-tank.getX() < Tank.HEIGHT/2
+                            && bomb.getX()-tank.getX() > -Tank.HEIGHT/2
+                            && bomb.getY()-tank.getY() < Tank.WIDTH/2
+                            && bomb.getY()-tank.getY() > -Tank.WIDTH/2) {
+                        bomb.isAlive = false;
+                        tank.isAlive = false;
+                    }
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    // detect movement
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // monitor direction change
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_W:
+                heroTank.setDirVH(Tank.DIR_V);
+                heroTank.setDirPosNeg(Tank.DIR_NEGATIVE);
+                break;
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_S:
+                heroTank.setDirVH(Tank.DIR_V);
+                heroTank.setDirPosNeg(Tank.DIR_POSITIVE);
+                break;
+            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_A:
+                heroTank.setDirVH(Tank.DIR_H);
+                heroTank.setDirPosNeg(Tank.DIR_NEGATIVE);
+                break;
+            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_D:
+                heroTank.setDirVH(Tank.DIR_H);
+                heroTank.setDirPosNeg(Tank.DIR_POSITIVE);
+                break;
+        }
+        heroTank.step();
+
+        // press J, heroTank shoot bomb
+        if (e.getKeyCode() == KeyEvent.VK_J) {
+            if (heroTank.bombs.size() < Tank.MAX_BOMB_NUM) {
+                Bomb thisBomb = heroTank.shoot();
+                Thread bombThread = new Thread(thisBomb);
+                bombThread.start();
+            }
+        }
+
+        repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    // check hits, remove dead objects and update display
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(16);       // display update interval
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // check if bombs of heroTank hit enemyTank
+            for (int i = 0; i < enemyTanks.size(); i++) {
+                EnemyTank enemyTank = enemyTanks.get(i);
+
+                if (enemyTank.isAlive) {
+                    for (int j = 0; j < heroTank.bombs.size(); j++) {
+                        Bomb heroBomb = heroTank.bombs.get(j);
+
+                        if (heroBomb.isAlive) {
+                            updateHit(enemyTank, heroBomb);
+                        }
+
+                        // remove dead heroBomb and enemyTank
+                        if (!heroBomb.isAlive) {
+                            heroTank.bombs.remove(heroBomb);
+                        }
+                        if (!enemyTank.isAlive) {
+                            enemyTanks.remove(enemyTank);
+                        }
+                    }
+                }
+            }
+
+            // check if bombs of enemyTank hit heroTank
+            if (heroTank.isAlive) {
+                for (int i = 0; i < enemyTanks.size(); i++) {
+                    EnemyTank enemyTank = enemyTanks.get(i);
+                    for (int j = 0; j < enemyTank.bombs.size(); j++) {
+                        Bomb enemyBomb = enemyTank.bombs.get(j);
+
+                        if (enemyBomb.isAlive) {
+                            updateHit(heroTank, enemyBomb);
+                        }
+
+                        // remove dead enemy bombs
+                        if (!enemyBomb.isAlive) {
+                            enemyTank.bombs.remove(enemyBomb);
+                        }
+                    }
+                }
+            }
+
+            repaint();
+        }
+    }
+}
+
